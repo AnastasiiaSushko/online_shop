988eae3af678d09e6a17abbfa52825d487ea20ef
anastasiia <anastasiiasushko22@gmail.com>
2017-01-18 00:17:05 +0200

    final

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,15 +8,19 @@
     <link rel="stylesheet" href="/css/style1.css">
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <link rel="stylesheet" href="http://getbootstrap.com/examples/carousel/carousel.css">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
 
 
     <!-- Custom styles for this template -->
@@ -24,7 +28,6 @@
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
 
 
 </head>
@@ -47,7 +50,7 @@
 
             @foreach($menu as $category)
                 @if($category->title=='Акция')
-            <li><a  href="/category/{{$category->id}}" style="color: red">{{$category->title}}</a> </li>
+                    <li><a href="/category/{{$category->id}}" style="color: red">{{$category->title}}</a></li>
                 @endif
             @endforeach
             <li class="dropdown">
@@ -59,34 +62,38 @@
 
                         @if($category->parent_id==0)
 
-                        <li class="menu-item dropdown dropdown-submenu"><a tabindex="-1"
+                            <li class="menu-item dropdown dropdown-submenu"><a tabindex="-1"
 
-                                                                           href="/category/{{$category->id}}">{{$category->title}}</a>
-                            <ul class="dropdown-menu">
-                            @foreach($menu as $category2)
+                                                                               href="/category/{{$category->id}}">{{$category->title}}</a>
+                                <ul class="dropdown-menu">
+                                    @foreach($menu as $category2)
 
-                                @if($category->id==$category2->parent_id)
+                                        @if($category->id==$category2->parent_id)
 
-                                <li class="menu-item "><a href="/category/{{$category2->id}}"> {{$category2->title}}</a></li>
+                                            <li class="menu-item "><a
+                                                        href="/category/{{$category2->id}}"> {{$category2->title}}</a>
+                                            </li>
 
-                                @endif
+                                        @endif
 
-                                @endforeach
-                                    </ul>
-                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
                         @endif
                     @endforeach
                 </ul>
             </li>
 
             @if (($user = Auth::user()) && $user->moderator)
-                <li><a href="/admin"   style="color:blue">Admin panel</a></li>
+                <li><a href="/admin" style="color:blue">Admin panel</a></li>
             @endif
 
-                @if (($user = Auth::user()))
-                    <li><a href="/user/{{$user->id}}">Личный кабинет</a></li>
-                @endif
-               <li><a href="/cart"><img src="http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/empty-cart-light.png" style="height: 30px"></a></li>
+            @if (($user = Auth::user()))
+                <li><a href="/user/{{$user->id}}">Личный кабинет</a></li>
+            @endif
+            <li><a href="/cart"><img
+                            src="http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/empty-cart-light.png"
+                            style="height: 30px"></a></li>
         </ul>
 
 
@@ -94,12 +101,12 @@
             <div class="input-group">
                 <input type="text" class="form-control" name="q" placeholder="Искать...">
                                     <span class="input-group-btn">
-                                        <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                        <button class="btn btn-default" type="submit"><span
+                                                    class="glyphicon glyphicon-search" aria-hidden="true"></span>
                                         </button>
                                     </span>
             </div><!-- /input-group -->
         </form>
-
 
 
         <ul class="nav navbar-nav navbar-right" style="margin-right: 40px">
@@ -146,33 +153,23 @@
                 <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
             </p>
             <div class="jumbotron" style="background: black; text-align: center">
-            <img src="/img/logo.png" >
+                <img src="/img/logo.png">
             </div>
 
-         </div>
-
+        </div>
 
 
         <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
             <div class="list-group">
                 <a href="#" class="list-group-item active">ТОП 5 ТОВАРА</a>
                 @foreach($top as $top1)
-                    <a href="/product/{{$top1->id}}" class="list-group-item">{{$top1->title}} <img src="{{$top1->main_photo}}" style="height: 60px;"></a>
+                    <a href="/product/{{$top1->id}}" class="list-group-item">{{$top1->title}} <img
+                                src="{{$top1->main_photo}}" style="height: 60px;"></a>
                 @endforeach
             </div>
         </div><!--/.sidebar-offcanvas-->
 
-
-
-
-
-
-
-
-
-
         @yield('content')
-
 
 
         <div class="col-xs-6 col-sm-3 sidebar-offcanvas">
@@ -183,8 +180,6 @@
     </div><!--/row-->
 
 
-
-
     <hr>
 
     <footer>
@@ -192,8 +187,6 @@
     </footer>
 
 </div><!--/.container-->
-
-
 
 
 <!-- Modal subscribe -->
@@ -216,27 +209,26 @@
 
 <script>
 
-    window.onbeforeunload = function(){return confirm('Покинуть страницу?')};
+    window.onbeforeunload = function () {
+        return confirm('Покинуть страницу?')
+    };
     var delay_popup = 15000;
     setTimeout("document.getElementById('parent_popup').style.display='block'", delay_popup);
 
 </script>
 
 
-
-
-
 <script src="/js/functions.js"></script>
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="http://getbootstrap.com/assets/js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+<script src="http://getbootstrap.com/assets/js/vendor/holder.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
 
 </body>
 </html>
